ef126e26fa080f50e077a70c3e5bdb44a72a2669
userRohit <rk2289868@gmail.com>
2025-08-05 08:12:40 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+if __name__ == '__main__':
+    result1 = add(10, 2)
+    result2 = sub(10, 2)
+    result3 = mul(10, 2)
+    result4 = div(10, 2)
+    print(result1, result2, result3, result4)
